654875b741b568ccde38342fab85e6715558575c
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-15 14:35:54 +0100

    written initial APIs

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require("cors");
+const { hashSync, compareSync } = require("bcrypt")
+const User = require("./models/database.model");
+const jwt = require("jsonwebtoken")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("views"))
+app.use(cors());
+
+app.get("/login", async (req, res) => {
+    res.render("login");
+})
+
+app.get("/", async (req, res) => {
+
+})
+
+app.post("/register", async (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: hashSync(req.body.password, 10)
+    })
+    user.save()
+        .then((user) => {
+            res.send({ success: true, user: { id: user._id, username: user.username } });
+        })
+        .catch((error) => {
+            res.send({ success: false, error })
+        })
+})
+
+app.post("/login", async (req, res) => {
+
+    User.findOne({ username: req.body.username })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).send({ success: false, message: "User not found" })
+            }
+            if (!compareSync(req.body.password, user.password)) {
+                return res.status(401).send({ success: false, message: "Incorrect Password" })
+            }
+            const payload = {
+                username: user.username, id: user._id
+            }
+            const token = jwt.sign(payload, "My secret", { expiresIn: "1d" });
+            res.status(200).send({ success: true, message: "Login Success", token: "Bearer " + token });
+        })
+})
+app.listen(8000, () =>
+    console.log("listening..."))
